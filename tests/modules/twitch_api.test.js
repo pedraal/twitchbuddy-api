32fb50680ca6b298dd02262b121278317aa48f31
pierre golfier <pierre@codeur.com>
2021-02-10 20:37:08 +0100

    add tests

--- tests/modules/twitch_api.test.js
+++ tests/modules/twitch_api.test.js
@@ -50,18 +50,57 @@ test('should prepare axios api instance using existing token', async () => {
   expect(api.axios.defaults.headers.common.Authorization).toBe(`Bearer ${token}`)
 })
 
-test('should return clips', async () => {
+test('Should retrieve channel id', async () => {
   const api = new TwitchApi()
   await api.setupAxios()
-
-  const { data } = await api.getAndPrepareClips({ channel: 'zerator' })
-  expect(data.length).toBeGreaterThan(0)
+  const id = await api.getChannelId('zerator')
+  expect(id.length).toBeGreaterThan(0)
 })
 
-test('should return replays', async () => {
+test('Should retrieve channel ids', async () => {
+  const api = new TwitchApi()
+  await api.setupAxios()
+  const users = await api.getChannelId(['toneeuw', 'woodspices'])
+  expect(users.length).toBeGreaterThan(0)
+  expect(users[0].id.length).toBeDefined()
+})
+
+test('Should retrieve clips', async () => {
   const api = new TwitchApi()
   await api.setupAxios()
   const channelId = await api.getChannelId('zerator')
-  const { data } = await api.getReplays(channelId)
-  expect(data.length).toBeGreaterThan(0)
+  const { data: clips, pagination } = await api.getClips({ broadcaster_id: channelId })
+  expect(clips.length).toBeGreaterThan(0)
+  expect(pagination).toBeDefined()
+})
+
+test('Should retrieve games', async () => {
+  const api = new TwitchApi()
+  await api.setupAxios()
+  const channelId = await api.getChannelId('zerator')
+  const { data: clips } = await api.getClips({ broadcaster_id: channelId })
+  const gameIds = clips.map(clip => clip.game_id)
+  const games = await api.getGames(gameIds)
+  expect(games.length).toBeGreaterThan(0)
+  expect(games[0].name).toBeDefined()
+})
+
+test('Should retrieve and prepare clips', async () => {
+  const api = new TwitchApi()
+  await api.setupAxios()
+  const { data: clips, pagination } = await api.getAndPrepareClips({ channel: 'zerator' })
+  expect(clips.length).toBeGreaterThan(0)
+  expect(clips[0].title).toBeDefined()
+  expect(clips[0].downloadLink).toBeDefined()
+  expect(clips[0].category).toBeDefined()
+  expect(pagination).toBeDefined()
+})
+
+test('Should retrieve replays', async () => {
+  const api = new TwitchApi()
+  await api.setupAxios()
+  const channelId = await api.getChannelId('zerator')
+  const { data: replays } = await api.getReplays(channelId)
+  expect(replays.length).toBeGreaterThan(0)
+  expect(replays[0].ended_at).toBeDefined()
 })
